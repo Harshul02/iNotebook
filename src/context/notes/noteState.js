42fab1e592d7c8f76505acc1d08edb630b013777
Harshul Jain <harshulgangwal02@gmail.com>
2022-11-24 15:26:05 +0530

    Added icons and styled card

--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -5,7 +5,7 @@ const NoteState = (props)=>{
 
     const initialNotes = [
         {
-          "_id": "637f190e5955689d54c84904",
+          "_id": "637f190e5f955689d54c84904",
           "user": "637f17c5390bdcac32780202",
           "title": "My Title",
           "description": "Please wake up early",
@@ -14,7 +14,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "637f191f5955689d54c84906",
+          "_id": "637f191f59fewf55689sdsd54c84906",
           "user": "637f17c5390bdcac32780202",
           "title": "My Personal Data",
           "description": "Please wake up early",
@@ -23,7 +23,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "637f191f5955689d54c84906",
+          "_id": "637f191f59fw55689d54c84906",
           "user": "637f17c5390bdcac32780202",
           "title": "My Personal Data",
           "description": "Please wake up early",
@@ -32,7 +32,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "637f191f5955689d54c84906",
+          "_id": "637f191f59fewf55689d54c84906",
           "user": "637f17c5390bdcac32780202",
           "title": "My Personal Data",
           "description": "Please wake up early",
