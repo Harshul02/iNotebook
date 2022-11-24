7fdbab154c4a921d5464f415ae55f29e8df1f084
Harshul Jain <harshulgangwal02@gmail.com>
2022-11-24 19:29:24 +0530

    Added Delete note method

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,10 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {Link, useLocation} from "react-router-dom";
 
  
 
 const Navbar = () => {
   let location = useLocation();
-  useEffect(() => {
-    
-    console.log(location);
-  }, [location]);
   return (
         <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -51,12 +51,14 @@ const NoteState = (props)=>{
       }
 
       //Delete a note
-      const deleteNote = ()=>{
-        
+      const deleteNote = (id)=>{
+        console.log(id);
+        const newNotes = notes.filter((note)=>{return note._id!==id})
+        setNotes(newNotes);
     }
 
       //Edit a note
-      const editNote = ()=>{
+      const editNote = (id, title, description, tag)=>{
         
     }
 
